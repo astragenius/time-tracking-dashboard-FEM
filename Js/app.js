a8e6807fa976c65630fc7d5610643c68f818fd53
astragenius <darthmalak86@gmail.com>
2022-05-21 20:18:55 +0200

    add - added showWeek and showMonth funktion and worked fine with render funktion

--- Js/app.js
+++ Js/app.js
@@ -30,12 +30,9 @@ const fetchData = fetch(url)
 const dailyBtn = document.getElementById('daily-btn');
 const weeklyBtn = document.getElementById('weekly-btn');
 const monthlyBtn = document.getElementById('monthly-btn');
-const mainTime = [...document.getElementsByClassName('box-main-time')];
-const boxFooter = [...document.getElementsByClassName('box-footer')];
 
 
 
-console.log(mainTime);
 
 
 
@@ -43,14 +40,19 @@ dailyBtn.addEventListener('click', showDay);
 weeklyBtn.addEventListener('click', showWeek);
 monthlyBtn.addEventListener('click', showMonth);
 
-function renderData(current, prev, index) {
-
-    console.log(mainTime[index])
+function renderData(current, prev, index, param) {
+    const mainTime = [...document.getElementsByClassName('box-main-time')];
+    const boxFooter = [...document.getElementsByClassName('time-last')];
+    
+   mainTime[index].textContent = `${current}hrs`;
+   boxFooter[index].textContent = `${prev}hrs`
+   boxFooter[index].previousElementSibling.textContent = `Last ${param} `;
 }
 
 function showDay() {
    
     const data = fetchData;
+    let param = 'Day';
     let dayPrev
     let dayCurrent
     
@@ -61,35 +63,58 @@ function showDay() {
             dayCurrent = element.timeframes.daily.current;
             dayPrev = element.timeframes.daily.previous;
             
-            renderData(dayCurrent, dayPrev, index)
+            renderData(dayCurrent, dayPrev, index, param)
 
         })
-           
-           
-      
-
+                 
     })
 
    
+}
+
+function showMonth() {
+   
+    const data = fetchData;
+    let param = 'Month';
+    let Prev
+    let Current
+    
+    data.then((res) => {
+        
+        return res.forEach((element, index) => {
+            
+            Current = element.timeframes.monthly.current;
+            Prev = element.timeframes.monthly.previous;
+            
+            renderData(Current, Prev, index, param)
+
+        })
+                 
+    })
+
    
 }
 
-function showWeek(data) {
+function showWeek() {
    
-    const newArr = []
-    newArr.push(data);
-    let newData = newArr[0].weekly.current; 
-   console.log(newData)
+    const data = fetchData;
+    let param = 'Week';
+    let dayPrev
+    let dayCurrent
+    
+    data.then((res) => {
+        
+        return res.forEach((element, index) => {
+            
+            dayCurrent = element.timeframes.weekly.current;
+            dayPrev = element.timeframes.weekly.previous;
+            
+            renderData(dayCurrent, dayPrev, index, param)
+
+        })
+                 
+    })
+
    
 }
-
-function showMonth(data) {
-   
-    const newArr = []
-    newArr.push(data);
-    let newData = newArr[0].monthly.current;
-    console.log(newData)
-   
-}
-
 
